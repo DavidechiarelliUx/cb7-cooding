73af569e82bc36f7949e567688ee1904a8e6f04a
Davide Chiarelli <121115353+DavidechiarelliUx@users.noreply.github.com>
2023-06-08 18:05:26 +0200

    08/06/2023

--- thriller movies/script.js
+++ thriller movies/script.js
@@ -1,7 +1,10 @@
 import { API_KEY, BASE_URL, API_URL, IMG_URL } from './utils/http.js';
-import { cE } from './utils/fn.js';
+import { cE, qS } from './utils/fn.js';
+import {navToggle, links } from './utils/nav.js';
 
-const main = document.getElementById('main');
+const rootEl = qS("#root");
+const headerEl = cE('header');
+const main = qS('#main'); 
 
 getMovies(API_URL);
 
@@ -9,7 +12,6 @@ function getMovies(url) {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log(data.results);
       showMovies(data.results);
     });
 }
@@ -18,48 +20,40 @@ function showMovies(data) {
   while (main.firstChild) {
     main.removeChild(main.firstChild);
   }
-
   data.forEach(movie => {
-    const { title, poster_path, vote_average, overview } = movie;
-
+    const { id, title, poster_path, vote_average, } = movie;
     const movieEl = cE('div');
     movieEl.classList.add('movie');
-
     const imgEl = cE('img');
     imgEl.src = IMG_URL + poster_path;
     imgEl.alt = title;
+    imgEl.setAttribute('data-id', id);
     movieEl.appendChild(imgEl);
-
-    const movieInfoEL =  cE ('div');
+    const movieInfoEL = cE('div');
     movieInfoEL.classList.add('movie_info');
-
     const titleEl = cE('div');
     titleEl.textContent = title;
     movieInfoEL.appendChild(titleEl);
-
     const ratingEL = cE ('span');
     ratingEL.textContent = vote_average;
     ratingEL.classList.add(getColor(vote_average));
     movieInfoEL.appendChild(ratingEL);
-
     movieEl.appendChild(movieInfoEL);
-
-    const overviewEl =cE('div');
-    overviewEl.classList.add('overview');
-
-    const overviewTitleEl= cE ('h3');
-    overviewTitleEl.textContent = 'Overview';
-    overviewEl.appendChild(overviewTitleEl);
-
-    const overviewTextEl = cE ('p');
-    overviewTextEl.textContent = overview;
-    overviewEl.appendChild(overviewTextEl);
-
-    movieEl.appendChild(overviewEl);
-    
+    const addToFavoritesButton = cE('button');
+    addToFavoritesButton.classList.add('add-to-favorites');
+    addToFavoritesButton.textContent = 'Aggiungi ai preferiti';
+    movieEl.appendChild(addToFavoritesButton);
     main.appendChild(movieEl);
 
-  })
+    imgEl.addEventListener('click', () => {
+      window.location.href = 'pages/description-movie.html?id=' + id;
+    });
+
+    addToFavoritesButton.addEventListener('click', () => {
+      addToFavoritesList(movie);
+      renderFavoritesList();
+    });
+  });
 }
 
 function getColor(vote) {
@@ -70,4 +64,68 @@ function getColor(vote) {
   } else {
     return 'red';
   }
+}
+
+function addToFavoritesList(movie) {
+  const favoritesList = JSON.parse(localStorage.getItem('favorites')) || [];
+  if (!favoritesList.some(favorite => favorite.id === movie.id)) {
+    favoritesList.push(movie);
+    localStorage.setItem('favorites', JSON.stringify(favoritesList));
+  }
+}
+
+function renderFavoritesList() {
+  const favoritesList = getFavoritesList();
+  const favoritesEl = qS('#favorites');
+
+  while (favoritesEl.firstChild) {
+    favoritesEl.removeChild(favoritesEl.firstChild);
+  }
+
+  favoritesList.forEach(movie => {
+    const { id, title, poster_path } = movie;
+    const favoriteEl = document.createElement('div');
+    favoriteEl.classList.add('favorite');
+    const imgEl = document.createElement('img');
+    imgEl.src = IMG_URL + poster_path;
+    imgEl.alt = title;
+    favoriteEl.appendChild(imgEl);
+    const starEl = document.createElement('span');
+    starEl.classList.add('star');
+    const starIcon =document.createTextNode('\u2605');
+    starEl.appendChild(starIcon);
+    favoriteEl.appendChild(starEl);
+    const titleEl = document.createElement('div');
+    titleEl.textContent = title;
+    titleEl.classList.add('titleFavorite');
+    favoriteEl.appendChild(titleEl);
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove-from-favorites');
+    removeButton.textContent = 'Rimuovi dai preferiti';
+    favoriteEl.appendChild(removeButton);
+    removeButton.addEventListener('click', () => {
+      removeFromFavoritesList(id);
+      renderFavoritesList();
+    });
+    imgEl.addEventListener('click', () => {
+      window.location.href = 'pages/description-movie.html?id=' + id;
+    });
+    favoritesEl.appendChild(favoriteEl);
+  });
+}
+
+function getFavoritesList() {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  return favorites;
+}
+
+function removeFromFavoritesList(movieId) {
+  const favoritesList = getFavoritesList();
+  const updatedFavoritesList = favoritesList.filter(movie => movie.id !== movieId);
+  localStorage.setItem('favorites', JSON.stringify(updatedFavoritesList));
+}
+
+function isFavorite(movieId) {
+  const favoritesList = getFavoritesList();
+  return favoritesList.some(movie => movie.id === movieId);
 }
